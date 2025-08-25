0ec7559f60f79b89378433449386a14b8d5ba3a7
Zartasha Kanwal <zartashakanwal529@gmail.com>
2025-08-26 01:18:52 +0500

    fix enlnt issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,11 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
@@ -20,6 +17,34 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "react/no-unescaped-entities": [
+        "error",
+        {
+          forbid: [
+            {
+              char: ">",
+              alternatives: ["&gt;"],
+            },
+            {
+              char: "}",
+              alternatives: ["&#125;"],
+            },
+          ],
+        },
+      ],
+    },
+  },
 ];
-
 export default eslintConfig;
+
+
+
+
+
+
+
+
+
+
--- src/Components/Sections/HeroSection.tsx
+++ src/Components/Sections/HeroSection.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./Header";
 import HeroLG from './HeroLG';
 import HeroMobile from './HeroMobile';
 
